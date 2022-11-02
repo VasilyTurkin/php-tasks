30318221769db02624bd0d3c04ba49b4a0448c6f
Vasily Turkin <turkin1051994@gmail.com>
2022-11-02 19:38:19 +0300

    Все переписал

---
+++ filesystem/сreatingFile.php
@@ -0,0 +1,44 @@
+<?php
+
+function set(string $key,  $value)
+{
+    $fileName = "storage.txt";
+    $arrayData = [];
+    $arrayData[$key] = $value;
+    $array = serialize($arrayData);
+    file_put_contents($fileName,$array,FILE_APPEND);
+
+    $myArray = file_get_contents($fileName);
+    $dataFile = unserialize($myArray);
+    foreach ($dataFile as $newKey => $oldValue) {
+        if($newKey === $key) {
+            $dataFile[$newKey] = $value;
+        } else {
+            $dataFile[$key] = $value;
+        }
+        file_put_contents($fileName, serialize($dataFile));
+    }
+
+}
+
+set("key1",1111111 );
+set("key2", "value2");
+set("key3", "value3");
+
+
+function get(string $key) {
+    $fileName = "storage.txt";
+    $myArray = file_get_contents($fileName);
+    $dataFileFile = unserialize($myArray);
+    foreach ($dataFileFile as $searchKey => $value) {
+        if($searchKey === $key) {
+            return $value;
+        }
+    }
+}
+
+var_dump(get("key1"));
+var_dump(get("key2"));
+var_dump(get("key3"));
+
+
