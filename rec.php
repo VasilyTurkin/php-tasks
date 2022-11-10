ea858ed7be08c11f56ba239a04911365484f7450
Vasily Turkin <turkin1051994@gmail.com>
2022-11-10 14:56:30 +0300

    Поправил поля и отступы

--- rec.php
+++ rec.php
@@ -1,34 +1,12 @@
 <?php
 
-//function fact(int $n)
-//{
-//    if ($n === 1) {
-//        return 1;
-//    }
-//    return $n * fact($n - 1);
-//}
-//
-//var_dump(fact(5));
-//
-//function fibo($i)
-//{
-//    if ($i == 0) return 0;
-//    if ($i == 1 || $i == 2) {
-//        return 1;
-//    } else {
-//        return fibo($i - 1) + fibo($i - 2);
-//    }
-//}
-//
-//var_dump(fibo(5));
+$arr = [2, 3, 5, 3];
 
-
-$arr  = [2,3,5,3];
-
-function sumArray(array $arr) {
+function sumArray(array $arr)
+{
     if (count($arr)) {
         $sum = array_shift($arr);
-       return  $sum + sumArray($arr);
+        return $sum + sumArray($arr);
     }
 
 }
